00f68e51df9ae1ce74a57d4df1c8811dd6f155f7
Vinay Kumar <kvinay1191@gmail.com>
2024-06-11 00:19:17 +0530

    Add string finder

---
+++ string-finder.js
@@ -0,0 +1,31 @@
+const isItemPresent = (item, str) => {
+  const itemArray = item.slice("");
+  for (let i = 0; i < itemArray.length; i++) {
+    if (!str.includes(itemArray[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const findString = (arr, str) => {
+  let longestStr = null;
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+    if (isItemPresent(item, str)) {
+      if (longestStr) {
+        if (longestStr.length < item.length) {
+          longestStr = item;
+        }
+      } else {
+        longestStr = item;
+      }
+    }
+  }
+  return longestStr;
+};
+
+const str1 = ["able", "apple", "monkey", "plea"];
+const str2 = "abpcplea";
+
+console.log(findString(str1, str2));
